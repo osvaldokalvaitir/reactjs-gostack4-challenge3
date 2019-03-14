af41189d584689ff505ea16b7a4135afea706c2b
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-03-14 12:07:16 -0300

    Novo projeto, ESLint, Redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,24 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
-import reducers from './ducks';
 import sagas from './sagas';
+import reducers from './ducks';
 
 const middlewares = [];
 
 const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
-
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 middlewares.push(sagaMiddleware);
 
-const createAppropriateStore = process.env.NODE_ENV === 'development' ? console.tron.createStore : createStore;
+const composer = process.env.NODE_ENV === 'development'
+  ? compose(
+    applyMiddleware(...middlewares),
+    console.tron.createEnhancer(),
+  )
+  : compose(applyMiddleware(...middlewares));
 
-const store = createAppropriateStore(reducers, compose(applyMiddleware(...middlewares)));
+const store = createStore(reducers, composer);
 
 sagaMiddleware.run(sagas);
 
